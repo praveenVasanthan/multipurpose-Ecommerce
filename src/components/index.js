e234a9a290ca603545c855e3d67c5764e33ff5db
praveenVasanthan <vasanth.dev.200@gmail.com>
2025-04-09 14:58:02 +0530

    products page

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,10 @@
 //////Components index
 
 export { default as TopBar } from "./TopBar";
+
+// Cards
+export { default as Card } from "./Card";
+// Product Filter
+export { default as Filter } from "./Filter";
+// Product control
+export { default as Control } from "./Control";
